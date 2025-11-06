f05507a821e9526d22384fe25ccac8b14e7d3b1a
Clément VANDEWALLE <clement.vandewalle@adeo.com>
2025-11-06 10:06:26 +0100

    add google verif

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="google-site-verification" content="VZKrlEwDzHB8p4y2kL15wCUqv75VZlBl3wKSAjAaRCU" />
+      <title>Mon site</title>
+    </>
+  );
+}
